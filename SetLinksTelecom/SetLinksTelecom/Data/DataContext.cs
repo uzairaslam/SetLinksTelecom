8795fe790d459448a6606b787bd4b8b3bc565147
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2019-01-17 20:21:09 +0500

    InitialCommit
    Initial Commit

---
+++ SetLinksTelecom/SetLinksTelecom/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SetLinksTelecom.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("name=SetLinksTelecomDBContext")
+        {
+            
+        }
+    }
+}
