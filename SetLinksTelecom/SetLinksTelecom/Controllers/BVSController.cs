9ef134316b53cb193fa76739589e66b2dc4bde07
uzairaslam - Uzair Aslam <uzairaslam64@gmail.com>
2019-03-01 16:11:57 +0500

    Bvs Services
    Bvs Services

---
+++ SetLinksTelecom/SetLinksTelecom/Controllers/BVSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SetLinksTelecom.Controllers
+{
+    public class BVSController : Controller //Biometric verified system
+    {
+        // GET: BVS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SetLinksTelecom/SetLinksTelecom/Models/BvsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SetLinksTelecom.Models
+{
+    public class BvsService
+    {
+        public int BvsServiceId { get; set; }
+        public string Name { get; set; }
+    }
+}
